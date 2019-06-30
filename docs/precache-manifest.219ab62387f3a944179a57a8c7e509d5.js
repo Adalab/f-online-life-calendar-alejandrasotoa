fa21ab80afacbeb985c76341cfefb96f9e34d2fe
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-06-30 22:59:56 +0200

    docs ready

---
+++ docs/precache-manifest.219ab62387f3a944179a57a8c7e509d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76450fde3bce24521a98c0419025bcb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e252196f4df0dc66eb2e",
+    "url": "./static/css/main.dc035f79.chunk.css"
+  },
+  {
+    "revision": "074e7f5f3f900664de64",
+    "url": "./static/js/2.c9f6fa90.chunk.js"
+  },
+  {
+    "revision": "e252196f4df0dc66eb2e",
+    "url": "./static/js/main.ce4a1192.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
